a13db2705ca50216a0b4c572326bc956acbfc7a5
JohnRFoster <fosterj@bu.edu>
2018-06-18 16:27:00 -0400

    get all days in series

--- Cary_pop/mice_lag_code.R
+++ Cary_pop/mice_lag_code.R
@@ -4,17 +4,24 @@ library(lubridate)
 dat <- read.csv("Cary_pop/tick_cleaned") # tick data
 gr <- read.csv("GreenTicks.csv", header = FALSE) # green control mice data
 
+### tick data
 green <- dat %>% select(c(Grid, DATE, n_larvae, n_nymphs, n_adults)) %>% 
   filter(Grid == "Green Control") # get only tick data from green control
 green$DATE <- ymd(green$DATE)
 
+
+### mouse data
 mice <- read.csv("Cary_pop/Cary_mouse.csv") %>% 
   filter(Grid == "Green Control")
 mice$Full.Date.1 <- ymd(mice$Full.Date.1)
+mice$Full.Date.2 <- ymd(mice$Full.Date.2)
 
-# get year-month for each sampling day (ticks and mice)
-day.mice <- unique(mice$Full.Date.1) # unique sampling days: mice
-day.tick <- unique(green$DATE) # unique sampling days: tick
+# unique trap 
+day.1 <- unique(mice$Full.Date.1) %>% as.character() # 1st capture event in mouse series
+day.2 <- unique(mice$Full.Date.2) %>% as.character() # 2nd capture event in mouse series
+
+day.mice <- c(rbind(day.1, day.2)) %>% ymd() # unique sampling days: mice
+day.tick <- green$DATE # unique sampling days: tick
 
 year.t1 <- vector()
 for(t in 1:length(day.tick)){
