eb4a17e50ff2f42695108ec0bc747b7369ad45c5
John R Foster <fosterj@bu.edu>
2019-06-04 16:11:43 -0400

    first pass at gdd_null one step prediction

---
+++ Functions/predict_state_one_gdd_null.R
@@ -0,0 +1,144 @@
+#' This is a one step ahead prediction for the GDD_Threshold model
+#' 
+#' Uncertainty is also partioned 
+#' 
+#' This model has interacting parameters in the matrix (phi*(1-theta))
+#' 
+#' Transition is a switch, on when over a threshold or within window, depending
+#' on function call
+#' 
+#' Survival in the questing matrix constant for each life stage      
+#'
+#' @param type type of uncertainty to simulate. One of "deterministic","ic","parameter","process"
+#' @param thresh one of "low" or "window"
+#' @param site site, one of "Green Control","Henry Control","Tea Control"
+#' @param params matrix of JAGS output of parameters
+#' @param ic matrix of JAGS output of states
+#' @param data model data fed to JAGS
+#' @param Nmc Number of mc samples
+#' @param draw Vector of random draws (row numbers) or sampling
+#' @export
+
+
+predict_state_one_gdd_null <- function(type, thresh, site, params, ic, data, Nmc, draw){
+  
+  # call data from model run for storage dimensions and indexing
+  if(site == "Green Control"){
+    s <- 1
+  } else if(site == "Henry Control"){
+    s <- 2
+  } else {
+    s <- 3
+  }
+  N_est <- data$N_est[s]
+  N_days <- data$N_days[s]
+  dt.index <- data$dt.index[s,]
+  df <- data$df[s,]
+
+  # storage
+  pred <- array(dim = c(3,N_est,Nmc))
+  A <- array(0, dim=c(3,3,N_days))
+  
+  # mean parameters
+  param.mean <- apply(params, 2, mean)
+  
+  # select appropriate initial conditions
+  if("ic" %in% type){
+    IC <- ic[draw,]
+  } else {
+    IC.mean <- apply(ic, 2, mean)
+    IC.names <- names(IC.mean)
+    IC <- matrix(IC.mean, Nmc, ncol = length(IC.mean), byrow = TRUE)
+    colnames(IC) <- IC.names
+  }
+  
+  # select appropriate parameters
+  if("parameter" %in% type){
+    phi.l.mu <- params[draw,"phi.l.mu"]
+    phi.n.mu <- params[draw,"phi.n.mu"]
+    phi.a.mu <- params[draw,"phi.a.mu"]
+    grow.ln.mu <- params[draw,"grow.ln.mu"]
+    grow.na.mu <- params[draw,"grow.na.mu"]
+    repro.mu <- params[draw,"repro.mu"]
+  } else {
+    phi.l.mu <- rep(param.mean["phi.l.mu"], Nmc)
+    phi.n.mu <- rep(param.mean["phi.n.mu"], Nmc)
+    phi.a.mu <- rep(param.mean["phi.a.mu"], Nmc)
+    grow.ln.mu <- rep(param.mean["grow.ln.mu"], Nmc)
+    grow.na.mu <- rep(param.mean["grow.na.mu"], Nmc)
+    repro.mu <- rep(param.mean["repro.mu"], Nmc)
+  }
+  
+  # select appropraite covariance matrix
+  if("process" %in% type){
+    SIGMA <- array(NA, dim = c(3,3,Nmc))
+    SIGMA[1,1,] <- params[draw,"SIGMA[1,1]"]
+    SIGMA[1,2,] <- params[draw,"SIGMA[1,2]"]
+    SIGMA[1,3,] <- params[draw,"SIGMA[1,3]"]
+    SIGMA[2,1,] <- params[draw,"SIGMA[2,1]"]
+    SIGMA[2,2,] <- params[draw,"SIGMA[2,2]"]
+    SIGMA[2,3,] <- params[draw,"SIGMA[2,3]"]
+    SIGMA[3,1,] <- params[draw,"SIGMA[3,1]"]
+    SIGMA[3,2,] <- params[draw,"SIGMA[3,2]"]
+    SIGMA[3,3,] <- params[draw,"SIGMA[3,3]"]
+    
+    # convert from precision to standard dev
+    for(i in 1:Nmc){
+      SIGMA[,,i] <- solve(SIGMA[,,i])
+    }
+  } else {
+    SIGMA <- array(0, dim = c(3,3,Nmc))
+  }
+  
+  # run mcmc sampling
+  for(m in 1:Nmc){
+    
+    phi.11 <- inv.logit(phi.l.mu[m])
+    phi.22 <- inv.logit(phi.n.mu[m])
+    theta.32[t] <- ifelse((gdd[t] <= 750) || (gdd[t] >= 2500),inv.logit(grow.na.mu[m]),0)
+    
+    if(thresh == "low"){
+      theta.21[t] <- ifelse((gdd[t] >= 500),inv.logit(grow.ln.mu[m]),0)  
+    } else {
+      theta.21[t] <- ifelse((gdd[t] >= 500) && (gdd[t] <= 2000),inv.logit(grow.ln.mu[m]),0)  
+    }
+    
+    # draw transition matrix
+    A[1,1,] <- phi.11*(1-theta.21)
+    A[1,3,] <- exp(repro.mu[m])
+    A[2,1,] <- phi.11*theta.21
+    A[2,2,] <- phi.22*theta.32
+    A[3,2,] <- phi.22*(1-theta.32)
+    A[3,3,] <- inv.logit(phi.a.mu[m])
+    
+    ## aggrigate transition matricies
+    for(t in 1:(N_est-1)){                 # loop over the number of sampling days - 1
+      if(t == 1){
+        TRANS <- A[,,1] %*% A[,,2]
+        for(d in 3:df[1]){
+          TRANS <- TRANS %*% A[,,d]
+        }
+      } else {
+        for(d in 1:df[t]){
+          if(d == 1){
+            TRANS <- A[,,dt.index[t-1]] %*% A[,,dt.index[t-1]+1]
+          } else {
+            TRANS <- TRANS %*% A[,,dt.index[t-1]+d]
+          }
+        }
+      }
+      
+      ## initial condition
+      l <- IC[m, paste("x[1,",t, "]",sep="")]
+      n <- IC[m, paste("x[2,",t, "]",sep="")]
+      a <- IC[m, paste("x[3,",t, "]",sep="")]
+      obs <- as.matrix(c(l,n,a),3,1)
+      Ex <- TRANS %*% obs
+      est.mvnorm <- rmvnorm(1,Ex,SIGMA[,,m])
+      pred[1,t,m] <- max(est.mvnorm[1], 0)
+      pred[2,t,m] <- max(est.mvnorm[2], 0)
+      pred[3,t,m] <- max(est.mvnorm[3], 0)
+    }
+  }
+  return(pred)
+}
