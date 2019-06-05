c7b0c91002257d2240923cf761987c2d0c39e49c
John R Foster <fosterj@bu.edu>
2019-06-05 15:14:05 -0400

    source model, change site for cluster run, correct file path for storage

--- Scripts/Run_Model.R
+++ Scripts/Run_Model.R
@@ -6,12 +6,11 @@ print("-----------------------------------------------------------------")
 
 library(ecoforecastR)
 
-# source("Functions/cary_tick_met_JAGS.R") # get data
-# source("Functions/site_data_met.R") # subset data for independent fits
-source("FinalOut/Models/GDD_Threshold_Independent.R") # model
+#### model ####
+source("Models/Temp_GDDThreshold_Independent.R") 
 
 ## model options
-site.run <- "Tea Control"      # site
+site.run <- "Green Control"      # site
 met.variable <- NULL          # met driver on survival
 n.adapt <- 150000                  # adaptive iterations
 n.chains <- 1                     # number of chains
@@ -21,13 +20,12 @@ iter2save <- 10000                 # number of iterations to save
 thin <- round(n.iter/iter2save)   # thinning interval
 
 ## file path to output folder
-out.folder <- "FinalOut/Independent_Fits/GDDThreshold/Window"
-out.name <- paste("GDDSwitch", gsub(" ","",site.run),sep="_")
+out.folder <- "../FinalOut/Independent_Fits/GDDThreshold/LowThreshOnly"
+out.name <- paste("Temp_GDDSwitch_gammaBeta33", gsub(" ","",site.run),sep="_")
 out.path <- file.path(out.folder,out.name)
 
 # compile and run model
 out <- run_model(site.run,n.adapt,n.chains,burnin,thin,n.iter)
-# out <- run_model(site.run,met.variable,n.adapt,n.chains,burnin,thin,n.iter)
 
 xx <- as.numeric(Sys.getenv("SGE_TASK_ID")) # read array job number to paste into output file
 save(out, file = paste(out.path, xx, ".RData", sep = ""))
