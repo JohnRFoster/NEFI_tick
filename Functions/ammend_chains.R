6de57d8893a3934dcdf87b4fd6e920644c5037dd
John R Foster <fosterj@bu.edu>
2019-07-02 10:27:43 -0400

    get to work for loop output

--- Functions/ammend_chains.R
+++ Functions/ammend_chains.R
@@ -1,31 +1,74 @@
 #' This takes the continued jags run and ammends it to the previous run
 #'
 #'@param dir file path to model output
-#'@param chain.numbs vector of chain numbers
+#'@param num.chains integer, the number of chains run in parallel
+#'@param num.out integer, the number of succesive files written out (the minimum between chains)
+#'@param thin number of iterations to keep, default 5000
+#'@param save do you want to save the combined chains? defult is no (NULL) 
+#'
 
-ammend_chains <- function(dir, out1, out2, thin = 10000, save = NULL){
-  # storage
-  params <- state <- state.m <- list()
+source('Functions/combine_chains.R')
+
+ammend_chains <- function(dir, num.chains, num.out, thin = 5000, save = NULL){
   
-  # JAGS output
-  part1 <- combine_chains(file.path(dir,out1))
-  part2 <- combine_chains(file.path(dir,out2))
-  
-  # number of chains
-  n.chains <- length(part1$params)
-  
-  for(c in 1:nchains){
-    params[[c]] <- as.mcmc(rbind(part1$params[[c]],part2$params[[c]]))
-    state[[c]] <- as.mcmc(rbind(part1$predict[[c]],part2$predict[[c]]))
-    state.m[[c]] <- as.mcmc(rbind(part1$predict.m[[c]],part2$predict.m[[c]]))
+  all.chains <- list()
+  for(c in 1:num.chains){
+    c1 <- list()
+    for(i in 1:num.out){
+      load <- paste(dir, "_c", c, "_", i, ".RData", sep = "")
+      load(load)
+      if(num.out == 1){
+        c1$params <- as.mcmc(out$out$params)
+        c1$predict <- as.mcmc(out$out$predict)
+        c1$m.cols <- as.mcmc(out$out$m.cols)
+      } else {
+        c1$params <- rbind(c1$params, as.mcmc(out$out$params))
+        c1$predict <- rbind(c1$predict, as.mcmc(out$out$predict))
+        c1$m.cols <- rbind(c1$m.cols, as.mcmc(out$out$m.cols))
+      }
+    }
+    iter <- dim(c1$params)[1]
+    if(iter <= 5000){
+      all.chains$params[[c]] <- as.mcmc(c1$params)
+      all.chains$predict[[c]] <- as.mcmc(c1$predict)
+      all.chains$m.cols[[c]] <- as.mcmc(c1$m.cols)
+    } else {
+      iter <- round(seq(1, by = iter/thin, length = thin))
+      all.chains$params[[c]] <- as.mcmc(c1$params[iter,])
+      all.chains$predict[[c]] <- as.mcmc(c1$predict[iter,])
+      all.chains$m.cols[[c]] <- as.mcmc(c1$m.cols[iter,])
+    }
   }
   
-  out <- list(params = as.mcmc(params),
-              predict = as.mcmc(state),
-              predict.m = as.mcmc(state.m))
+  out.test <- list(params = as.mcmc(all.chains$params),
+                   predict = as.mcmc(all.chains$predict),
+                   m.cols = as.mcmc(all.chains$m.cols))
   
   if(!is.null(save)){
-    save(out,file.path(dir,save))
+    save(out.test, file = file.path(dir,save))
   }
-  return(out)
+  return(out.test)
 }
+
+
+# load("../FinalOut/HB_Partial_GDD/Temp/WindowLoop/GDDSwitch_Window_Temp_c1_1.RData")
+# load("../FinalOut/HB_Partial_GDD/Temp/WindowLoop/GDDSwitch_Window_Temp_c1_2.RData")
+# c1 <- list()
+# c1[[1]] <- out
+# c1[[2]] <- out
+# 
+# num.chains <- 5
+# num.out <- 3
+# dir <- "../FinalOut/HB_Partial_GDD/Temp/WindowLoop"
+# model <- "GDDSwitch_Window_Temp"
+# 
+# part1 <- combine_chains(file.path(dir,model))
+
+
+
+
+
+
+
+
+
