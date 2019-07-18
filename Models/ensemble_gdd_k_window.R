6427053c7d6ea17ea3c6b6918882d1c6a760ec4e
John R Foster <fosterj@bu.edu>
2019-07-18 11:24:02 -0400

    zero inflation outside of function, make N_days number of forecasts, remove unused code

--- Models/ensemble_gdd_k_window.R
+++ Models/ensemble_gdd_k_window.R
@@ -8,18 +8,11 @@ ensemble_gdd_k_window <- function(site, params, ic, gdd, N_days, Nmc){
   # call data from model run for storage dimensions and indexing
   if(site == "Green Control"){
     s <- 1
-    t <- 72
   } else if(site == "Henry Control"){
     s <- 2
-    t <- 72
   } else {
     s <- 3
-    t <- 72
   }
-  
-  # df <- rep(df, N_est)
-  # dt.index <- cumsum(df)
-  # N_days <- dt.index[length(dt.index)]
   
   # storage
   pred <- array(dim = c(3,N_days,Nmc))
@@ -39,10 +32,7 @@ ensemble_gdd_k_window <- function(site, params, ic, gdd, N_days, Nmc){
   k.l2n.high <- params[, "k.l2n.high"]
   k.n2a.low <- params[, "k.n2a.low"]
   k.n2a.high <- params[, "k.n2a.high"]
-  theta.larvae <- params[, "theta.larvae"]
-  theta.nymph <- params[, "theta.nymph"]
-  theta.adult <- params[, "theta.adult"]
-
+  
   SIGMA <- array(NA, dim = c(3,3,Nmc))
   SIGMA[1,1,] <- params[, "SIGMA[1,1]"]
   SIGMA[1,2,] <- params[, "SIGMA[1,2]"]
@@ -58,14 +48,6 @@ ensemble_gdd_k_window <- function(site, params, ic, gdd, N_days, Nmc){
   for(i in 1:Nmc){
     SIGMA[,,i] <- solve(SIGMA[,,i])
   }
-  
-  # random error
-  # tau.13 <- 1/sqrt(params[draw, "tau.13"])
-  # tau.21 <- 1/sqrt(params[draw, "tau.21"])
-  # tau.33 <- 1/sqrt(params[draw, "tau.33"])
-  # alpha.13 <- rnorm(Nmc, 0, tau.13)
-  # alpha.21 <- rnorm(Nmc, 0, tau.21)
-  # alpha.33 <- rnorm(Nmc, 0, tau.33)
   
   # run mcmc sampling
   for(m in 1:Nmc){
@@ -85,28 +67,29 @@ ensemble_gdd_k_window <- function(site, params, ic, gdd, N_days, Nmc){
     A[3,2,] <- phi.22*theta.32
     A[1,3,] <- exp(repro.mu[m] + alpha.13[m])
     
-    ## binary outcome of observation by life stage and
-    ## blend the poisson and zero inflation models
-    inflate.larvae <- ic[1,1]*rbinom(1, 1, theta.larvae[m]) + 0.1
-    inflate.nymph <- ic[2,1]*rbinom(1, 1, theta.nymph[m]) + 0.1
-    inflate.adult <- ic[3,1]*rbinom(1, 1, theta.adult[m]) + 0.1
+    
     
     # zero inflate????
     Nprev <- matrix(NA, 3, 1)
-    Nprev[1,1] <- rpois(1, inflate.larvae)
-    Nprev[2,1] <- rpois(1, inflate.nymph)
-    Nprev[3,1] <- rpois(1, inflate.adult)
-    # Nprev[1,1] <- rpois(1, ic[1,1])
-    # Nprev[2,1] <- rpois(1, ic[2,1])
-    # Nprev[3,1] <- rpois(1, ic[3,1])
+    # Nprev[1,1] <- rpois(1, inflate.larvae)
+    # Nprev[2,1] <- rpois(1, inflate.nymph)
+    # Nprev[3,1] <- rpois(1, inflate.adult)
+    Nprev[1,1] <- rpois(1, ic[1,m])
+    Nprev[2,1] <- rpois(1, ic[2,m])
+    Nprev[3,1] <- rpois(1, ic[3,m])
     
     ## aggrigate transition matricies
-    TRANS <- A[,,1] %*% A[,,2]
+    TRANS <- A[,,1] 
     
-    for(t in 3:N_days){
-      TRANS <- TRANS %*% A[,,d]
-    }
-      
+    for(t in 1:N_days){
+      if(t == 2){
+        TRANS <- TRANS %*% A[,,2]
+      } else if(t >= 3){
+        TRANS <- TRANS %*% A[,,2]
+        for(d in 3:t){
+          TRANS <- TRANS %*% A[,,d]
+        }
+      }
       # predict questing ticks
       Ex <- TRANS %*% Nprev
       
@@ -121,6 +104,7 @@ ensemble_gdd_k_window <- function(site, params, ic, gdd, N_days, Nmc){
       Nprev[1,1] <- pred[1,t,m]
       Nprev[2,1] <- pred[2,t,m]
       Nprev[3,1] <- pred[3,t,m]
+    }
   }
   return(pred)
 } # close function
