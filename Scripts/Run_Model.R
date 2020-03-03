b811e4705715138447aea8b5ba51cf00715ae97b
John R Foster <fosterj@bu.edu>
2020-03-03 14:58:15 -0500

    ran for individual site not HB

--- Scripts/Run_Model.R
+++ Scripts/Run_Model.R
@@ -1,38 +1,35 @@
-print("                                                                 ")
-print("-----------------------------------------------------------------")
-print("                         Start SCRIPT                            ")
-print("-----------------------------------------------------------------")
-
-
 library(rjags)
 
 #### model ####
-source("Models/GDD_Threshold_HB_Window.R") 
+source("Models/GDD_Null_K_All.R") 
+
+n.adapt <- 50000
+n.chains <- 3
 
 ## model options
-# site.run <- "Green Control"      # site
 met.variable <- NULL          # met driver on survival
-n.adapt <- 150000                  # adaptive iterations
-n.chains <- 1                     # number of chains
-burnin <- 50000                   # burnin iterations
-n.iter <- 750000                  # number of iterations post burnin
-iter2save <- 10000                 # number of iterations to save 
-thin <- round(n.iter/iter2save)   # thinning interval
 
 ## file path to output folder
-out.folder <- "../FinalOut/HB_Partial_GDD"
+out.folder <- "../FinalOut/Independent_Fits/GDDThreshold/K_all"
+#out.folder <- "../FinalOut/HB_Partial_GDD/K_estimate"
 
-# out.name <- paste("GDDSwitch_low", gsub(" ","",site.run),sep="_")
-out.name <- "GDDSwitch_Window"
 
+### Individual Runs ###
+site.run <- "Tea Control"      # site
+out.name <- paste("NULL_K_est_multiProc", gsub(" ","",site.run),sep="_")
 out.path <- file.path(out.folder,out.name)
+file <- paste(out.path, ".RData", sep = "")
 
-# compile and run model
-# out <- run_model(site.run,n.adapt,n.chains,burnin,thin,n.iter)
-out <- run_model(n.adapt,n.chains,burnin,thin,n.iter)
+out <- run_model(site.run, n.adapt, n.chains, file)
 
-xx <- as.numeric(Sys.getenv("SGE_TASK_ID")) # read array job number to paste into output file
-save(out, file = paste(out.path, xx, ".RData", sep = ""))
+
+### Hierarchical Runs ###
+# out.name <- "GDDSwitch_Low_k"
+# out <- run_model(n.adapt,n.chains,burnin,thin,n.iter)
+
+
+# xx <- as.numeric(Sys.getenv("SGE_TASK_ID")) # read array job number to paste into output file
+save(out, file = file)
 
 print("-----------------------------------------------------------------")
 print("                           END SCRIPT                            ")
