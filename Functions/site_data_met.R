831770b67ab3c14b50dbbe05f37c2fa4dd3f1058
John R Foster <fosterj@bu.edu>
2020-03-03 14:55:27 -0500

    more else ifs for getting different met data

--- Functions/site_data_met.R
+++ Functions/site_data_met.R
@@ -1,5 +1,5 @@
-
-site_data_met <- function(site, met.variable, data){
+library(plantecophys)
+site_data_met <- function(site, met.variable, data, obs.model = TRUE){
   # index for site
   dat.jags <- list()
   if(site == "Green Control"){
@@ -21,15 +21,38 @@ site_data_met <- function(site, met.variable, data){
   # dat.jags$year <- data$year[,s]
   
   if(!is.null(met.variable)){
-    if(met.variable == "temp"){
-      dat.jags$met <- data$met[,1,s]
-      dat.jags$temp.mis <- data$temp.mis[,s]
-    } else if(met.variable == "rh"){
-      dat.jags$met <- data$met[,2,s]
-      dat.jags$rh.mis <- data$rh.mis[,s]
-    } else {
-      dat.jags$met <- data$met[,3,s]
+    if(met.variable == "max temp"){
+      dat.jags$met <- data$temp.max[,s]
+      dat.jags$met.mis <- data$mis.temp.max[,s]
+    } else if(met.variable == "min temp"){
+      dat.jags$met <- data$temp.min[,s]
+      dat.jags$met.mis <- data$mis.temp.min[,s]
+    } else if(met.variable == "vpd"){
+      dat.jags$met <- data$vpd[,s]
+      dat.jags$met.mis <- data$mis.vpd[,s]
+    } else if(met.variable == "max rh"){
+      dat.jags$met <- data$rh.max[,s]
+      dat.jags$met.mis <- data$mis.rh.max[,s]
+    } else if(met.variable == "min rh"){
+      dat.jags$met <- data$rh.min[,s]
+      dat.jags$met.mis <- data$mis.rh.min[,s]
+    } else if(met.variable == "precip"){
+      dat.jags$met <- data$precip[,s]
     }
+    dat.jags$met.range <- range(dat.jags$met, na.rm = TRUE)
+  }
+  
+  
+  
+  if(obs.model){
+    obs.index <- c(1, dat.jags$dt.index)
+    met.obs <- data$temp.min[obs.index[1:dat.jags$N_est],s]
+    met.obs.miss <- which(is.na(met.obs))
+    met.obs.range <- range(met.obs, na.rm = TRUE)
+    
+    dat.jags$met.obs <- met.obs
+    dat.jags$met.obs.miss <- met.obs.miss
+    dat.jags$met.obs.range <- met.obs.range
   }
   return(dat.jags)
 }
