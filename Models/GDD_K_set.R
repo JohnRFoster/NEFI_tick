3498d8169e1e25cf8ed6d26bd3b7d3407e88e5d2
John R Foster <fosterj@bu.edu>
2020-03-16 10:23:44 -0400

    one beta for each obs production run

--- Models/GDD_K_set.R
+++ Models/GDD_K_set.R
@@ -69,14 +69,14 @@ run_model <- function(site.run, met.proc, n.adapt, n.chains){
                # "theta.nymph",
                # "theta.adult",
                "beta.l.obs",
-               "beta.l.lat",
+               # "beta.l.lat",
                # "beta.l.vert")
                "beta.n.obs",
-               "beta.n.lat",
-               "beta.n.vert",
-               "beta.a.obs",
-               "beta.a.lat",
-               "beta.a.vert")
+               # "beta.n.lat",
+               # "beta.n.vert",
+               "beta.a.obs")
+               # "beta.a.lat",
+               # "beta.a.vert")
   
   model = " model {
   
@@ -96,11 +96,11 @@ run_model <- function(site.run, met.proc, n.adapt, n.chains){
   beta.n.obs ~ dnorm(0, 0.001) T(1E-10,)
   beta.a.obs ~ dnorm(0, 0.001) T(1E-10,)
   # beta.l.vert ~ dnorm(0, 0.001) T(0,)
-  beta.n.vert ~ dnorm(0, 0.001) T(0,)
-  beta.a.vert ~ dnorm(0, 0.001) T(0,)
-  beta.l.lat ~ dnorm(0, 0.001)
-  beta.n.lat ~ dnorm(0, 0.001)
-  beta.a.lat ~ dnorm(0, 0.001)
+  # beta.n.vert ~ dnorm(0, 0.001) T(0,)
+  # beta.a.vert ~ dnorm(0, 0.001) T(0,)
+  # beta.l.lat ~ dnorm(0, 0.001)
+  # beta.n.lat ~ dnorm(0, 0.001)
+  # beta.a.lat ~ dnorm(0, 0.001)
   # theta.larva ~ dunif(0,1)
   # theta.nymph ~ dunif(0,1)
   # theta.adult ~ dunif(0,1)
@@ -183,9 +183,9 @@ run_model <- function(site.run, met.proc, n.adapt, n.chains){
   m[3,t] <- x[3,t]*b.adult[t] + 1E-10
   
   ## observation probability based on temperature
-  theta.larva[t] <- 1 / (1 + beta.l.obs*(met.obs[t] + beta.l.lat)^2)
-  theta.nymph[t] <- 1 / (1 + beta.n.vert + beta.n.obs*(met.obs[t] + beta.n.lat)^2)
-  theta.adult[t] <- 1 / (1 + beta.a.vert + beta.a.obs*(met.obs[t] + beta.a.lat)^2)
+  theta.larva[t] <- 1 / (1 + beta.l.obs*(met.obs[t])^2)
+  theta.nymph[t] <- 1 / (1 + beta.n.obs*(met.obs[t])^2)
+  theta.adult[t] <- 1 / (1 + beta.a.obs*(met.obs[t])^2)
   
   ## binary outcome of observation by life stage
   b.larva[t] ~ dbern(theta.larva[t])
