1833f45cac41a41ca01464178127f779b2cc6c6e
John R Foster <fosterj@bu.edu>
2020-03-19 16:27:58 -0400

    return functions for each life stage

--- Functions/obs_prob.R
+++ Functions/obs_prob.R
@@ -1,57 +1,54 @@
 
 
-obs_prob <- function(ua, obs.temp = NULL){
+obs_prob <- function(ua, N_est, obs.temp = NULL){
   
   monitor <- names(ua)
+  obs.driver <- obs.temp
   
   # larva obs
   if("theta.larva" %in% monitor){
-    theta.larva <- ua$theta.larva
-  } else {
-    for(t in seq_along(obs.temp)){
-      if ("beta.l.obs" %in% monitor & !("beta.l.lat" %in% monitor) & !("beta.l.vert" %in% monitor)){
-        theta.larva <- 1 / (1 + ua$beta.l.obs*obs.temp[t]^2)
-      } else if ("beta.l.obs" %in% monitor & "beta.l.lat" %in% monitor & !("beta.l.vert" %in% monitor)){
-        theta.larva <- 1 / (1 + ua$beta.l.obs*(ua$beta.l.lat + obs.temp[t])^2)
-      } else if ("beta.l.obs" %in% monitor & "beta.l.lat" %in% monitor & "beta.l.vert" %in% monitor){
-        theta.larva <- 1 / (1 + ua$beta.l.vert + ua$beta.l.obs*(ua$beta.l.lat + obs.temp[t])^2)
-      }
-    }
+    theta.larva <- function(){ua$theta.larva}
+  } else if ("beta.l.obs" %in% monitor & !("beta.l.lat" %in% monitor) & !("beta.l.vert" %in% monitor)){
+    theta.larva <- function(){1 / (1 + ua$beta.l.obs*obs.temp^2)}
+  } else if ("beta.l.obs" %in% monitor & "beta.l.lat" %in% monitor & !("beta.l.vert" %in% monitor)){
+    theta.larva <- function(){1 / (1 + ua$beta.l.obs*(ua$beta.l.lat + obs.temp)^2)}
+  } else if ("beta.l.obs" %in% monitor & "beta.l.lat" %in% monitor & "beta.l.vert" %in% monitor){
+    theta.larva <- function(){1 / (1 + ua$beta.l.vert + ua$beta.l.obs*(ua$beta.l.lat + obs.temp)^2)}
   }
   
   # nymph obs
   if("theta.nymph" %in% monitor){
-    theta.nymph <- ua$theta.nymph
-  } else {
-    for(t in seq_along(obs.temp)){
-      if ("beta.n.obs" %in% monitor & !("beta.n.lat" %in% monitor) & !("beta.n.vert" %in% monitor)){
-        theta.nymph <- 1 / (1 + ua$beta.n.obs*obs.temp[t]^2)
-      } else if ("beta.n.obs" %in% monitor & "beta.n.lat" %in% monitor & !("beta.n.vert" %in% monitor)){
-        theta.nymph <- 1 / (1 + ua$beta.n.obs*(ua$beta.n.lat + obs.temp[t])^2)
-      } else if ("beta.n.obs" %in% monitor & "beta.n.lat" %in% monitor & "beta.n.vert" %in% monitor){
-        theta.nymph <- 1 / (1 + ua$beta.n.vert + ua$beta.n.obs*(ua$beta.n.lat + obs.temp[t])^2)
-      }
-    }
+    theta.nymph <- function(){ua$theta.nymph}
+  } else if ("beta.n.obs" %in% monitor & !("beta.n.lat" %in% monitor) & !("beta.n.vert" %in% monitor)){
+    theta.nymph <- function(){1 / (1 + ua$beta.n.obs*obs.temp^2)}
+  } else if ("beta.n.obs" %in% monitor & "beta.n.lat" %in% monitor & !("beta.n.vert" %in% monitor)){
+    theta.nymph <- function(){1 / (1 + ua$beta.n.obs*(ua$beta.n.lat + obs.temp)^2)}
+  } else if ("beta.n.obs" %in% monitor & "beta.n.lat" %in% monitor & "beta.n.vert" %in% monitor){
+    theta.nymph <- function(){1 / (1 + ua$beta.n.vert + ua$beta.n.obs*(ua$beta.n.lat + obs.temp)^2)}
   }
   
   # adult obs
   if("theta.adult" %in% monitor){
-    theta.adult <- ua$theta.adult
-  } else {
-    for(t in seq_along(obs.temp)){
-      if ("beta.a.obs" %in% monitor & !("beta.a.lat" %in% monitor) & !("beta.a.vert" %in% monitor)){
-        theta.adult <- 1 / (1 + ua$beta.a.obs*obs.temp[t]^2)
-      } else if ("beta.a.obs" %in% monitor & "beta.a.lat" %in% monitor & !("beta.a.vert" %in% monitor)){
-        theta.adult <- 1 / (1 + ua$beta.a.obs*(ua$beta.a.lat + obs.temp[t])^2)
-      } else if ("beta.a.obs" %in% monitor & "beta.a.lat" %in% monitor & "beta.a.vert" %in% monitor){
-        theta.adult <- 1 / (1 + ua$beta.a.vert + ua$beta.a.obs*(ua$beta.a.lat + obs.temp[t])^2)
-      }
-    }
+    theta.adult <- function(){ua$theta.adult}
+  } else if ("beta.a.obs" %in% monitor & !("beta.a.lat" %in% monitor) & !("beta.a.vert" %in% monitor)){
+    theta.adult <- function(){1 / (1 + ua$beta.a.obs*obs.temp^2)}
+  } else if ("beta.a.obs" %in% monitor & "beta.a.lat" %in% monitor & !("beta.a.vert" %in% monitor)){
+    theta.adult <- function(){1 / (1 + ua$beta.a.obs*(ua$beta.a.lat + obs.temp)^2)}
+  } else if ("beta.a.obs" %in% monitor & "beta.a.lat" %in% monitor & "beta.a.vert" %in% monitor){
+    theta.adult <- function(){1 / (1 + ua$beta.a.vert + ua$beta.a.obs*(ua$beta.a.lat + obs.temp)^2)}
   }
   
-  return(list(theta.larva = theta.larva,
-              theta.nymph = theta.nymph,
-              theta.adult = theta.adult))
+  larva.prob <- nymph.prob <- adult.prob <- matrix(NA, length(ua$deviance), N_est)
+  for(t in 1:N_est){
+    obs.temp <- obs.driver[t]
+    larva.prob[,t] <- theta.larva()
+    nymph.prob[,t] <- theta.nymph()
+    adult.prob[,t] <- theta.adult()
+  }
+  
+  return(list(theta.larva = larva.prob,
+              theta.nymph = nymph.prob,
+              theta.adult = adult.prob))
 }
 
 
