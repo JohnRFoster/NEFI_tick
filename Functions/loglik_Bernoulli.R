3ea9025b710e2b024ffd058ccbfe5b5700b33192
John R Foster <fosterj@bu.edu>
2020-04-16 10:27:57 -0400

    Bernoulli log likelihood

---
+++ Functions/loglik_Bernoulli.R
@@ -0,0 +1,7 @@
+loglik_Bernoulli = function(theta, x) {
+  # theta   success probability parameter
+  # x       vector of data
+  n = length(x)
+  ans = log(theta)*sum(x)+log(1-theta)*(n-sum(x))
+  return(ans)
+}
