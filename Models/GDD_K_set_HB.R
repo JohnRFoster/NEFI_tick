9fb35f8d1f6c7b76459702db60e358417b6bbe36
John R Foster <fosterj@bu.edu>
2020-06-19 11:43:27 -0400

    clean up, fix inits and missing temp

--- Models/GDD_K_set_HB.R
+++ Models/GDD_K_set_HB.R
@@ -25,44 +25,48 @@ source("Functions/get_survival.R") # run mcmc and check for convergence / burnin
 source("Functions/hb_restructure.R")
 source("Functions/get_inits_HB.R")
 
-run_model <- function(met.proc, n.adapt, n.chains){
+run_model <- function(met.proc, n.adapt, n.chains,init.dir,init.model.name){
   
   data <- cary_ticks_met_JAGS()
-  data <- hb_restructure(data)
+  data <- hb_restructure(data, met.proc)
   data$R <- diag(1, 3, 3)
   
-  data$obs.temp.range <- range(data$temp.min, na.rm = TRUE)
-  
   # get survival estimates
-  survival <- get_survival(larva.driver = met.proc,
-                           nymph.driver = met.proc)
+  survival <- get_survival(larva.driver = NULL,
+                           nymph.driver = NULL)
   data$larva.mean <- survival$larva.survival.mean
   data$larva.prec <- survival$larva.survival.prec
   data$nymph.mean <- survival$nymph.survival.mean
   data$nymph.prec <- survival$nymph.survival.prec
   
+  # data$nymph.beta.mu <- survival$nymph.beta.mean
+  # data$nymph.beta.prec <- survival$nymph.beta.prec
+
+  
   # get inits from independent models
-  init.params <- get_inits_HB(met.proc)
+  init.params <- get_inits_HB(init.dir, init.model.name, met.proc)
   
-  inits <- function(){list(p = data$y[,-1,],
-                           repro.mu = rnorm(1, init.params["repro.mu"], 1),
-                           phi.l.mu = rnorm(1, init.params["phi.l.mu"], 0.01),
-                           phi.n.mu = rnorm(1, init.params["phi.n.mu"], 0.01),
-                           phi.a.mu = rnorm(1, init.params["phi.a.mu"], 0.01),
-                           grow.ln.mu = rnorm(1, init.params["grow.ln.mu"], 0.1),
-                           grow.na.mu = rnorm(1, init.params["grow.na.mu"], 0.1),
-                           theta.nymph = runif(1, init.params["theta.nymph"]*0.9, 0.9999),
-                           theta.adult = runif(1, init.params["theta.adult"]*0.9, 0.9999),
-                           alpha.a = rnorm(3, 0, 1))}
+  inits <- function() {
+    list(
+      p = data$y[, -1, ],
+      repro.mu = rnorm(1, init.params["repro.mu"], 1),
+      phi.l.mu = rnorm(1, init.params["phi.l.mu"], 0.01),
+      phi.n.mu = rnorm(1, init.params["phi.n.mu"], 0.01),
+      phi.a.mu = rnorm(3, init.params["phi.a.mu"], 0.01),
+      grow.ln.mu = rnorm(1, init.params["grow.ln.mu"], 0.1),
+      grow.na.mu = rnorm(1, init.params["grow.na.mu"], 0.1),
+      theta.nymph = runif(1, init.params["theta.nymph"] * 0.9, 0.9999),
+      theta.adult = runif(1, init.params["theta.adult"] * 0.9, 0.9999)
+    )
+  }
   
-  data$temp.max <- data$rh.max <- data$rh.min <- data$precip <- data$vpd <- NULL
-  data$mis.rh.max <- data$mis.rh.min <- data$mis.temp.max <- data$mis.vpd <- NULL
-  
+
   monitor <- c("x",
                "deviance",
                "phi.l.mu",
                "phi.n.mu",
                "phi.a.mu",
+               # "beta.n",
                "grow.ln.mu",
                "grow.na.mu",
                "repro.mu",
@@ -79,50 +83,43 @@ run_model <- function(met.proc, n.adapt, n.chains){
                # "beta.a.obs",
                # "beta.a.lat",
                # "beta.a.vert")
-               "alpha.a",
-               "alpha.n",
-               "tau.a",
-               "tau.n")
+               # "alpha.a",
+               "tau.a")
   
   model = " model {
   
   ### global hyperpriors
   phi.l.mu ~ dnorm(larva.mean,larva.prec)               # larvae survival
   phi.n.mu ~ dnorm(nymph.mean,nymph.prec)               # nymph survival
-  phi.a.mu ~ dnorm(5,1)               # adult survival
+  # phi.a.mu ~ dnorm(5,1)               # adult survival
   grow.ln.mu ~ dnorm(-6, 1)             # larvae -> nymph transition 
   grow.na.mu ~ dnorm(-6, 1)             # nymph -> adult transition
-  repro.mu ~ dnorm(7, 0.01) T(0,)              # adult -> larvae transition (reproduction)
+  repro.mu ~ dnorm(7, 1) T(0,)              # adult -> larvae transition (reproduction)
   
   ### precision priors
   SIGMA ~ dwish(R, 4)          # mvn [3 x 3] site process
   tau.a ~ dgamma(0.001, 0.001) # adult survival random effect 
-  tau.n ~ dgamma(0.001, 0.001) # nymph survival random effect 
+
+  ### coefficient on larva survival driven by met
+  # beta.n ~ dnorm(nymph.beta.mu, nymph.beta.prec)
 
   ### random effect prior
   for(s in 1:3){
-    alpha.a[s] ~ dnorm(0, tau.a)
-    alpha.n[s] ~ dnorm(0, tau.n)
+    phi.a.mu[s] ~ dnorm(5, tau.a)
   }
   
   ## observation regression priors
   beta.l.obs ~ dnorm(0, 0.001) T(1E-10,)
-  # beta.n.obs ~ dnorm(0, 0.001) T(1E-10,)
-  # beta.a.obs ~ dnorm(0, 0.001) T(1E-10,)
-  # beta.l.vert ~ dnorm(0, 0.001) T(0,)
-  # beta.n.vert ~ dnorm(0, 0.001) T(0,)
-  # beta.a.vert ~ dnorm(0, 0.001) T(0,)
-  # beta.l.lat ~ dnorm(0, 0.001)
-  # beta.n.lat ~ dnorm(0, 0.001)
-  # beta.a.lat ~ dnorm(0, 0.001)
-  # theta.larva ~ dunif(0,1)
   theta.nymph ~ dunif(0,1)
   theta.adult ~ dunif(0,1)
 
   ## missing min temp (observation temp)
   for(m in mis.temp.min){
-    temp.min[m] ~ dunif(obs.temp.range[1], obs.temp.range[2])
+    temp.min[m] ~ dunif(temp.min.range[1], temp.min.range[2])
   }
+  # for(m in met.mis){
+  #   met[m] ~ dunif(met.mis.range[1], met.mis.range[2])
+  # }
   
   ### first latent process 
   for(s in 1:3){
@@ -131,8 +128,8 @@ run_model <- function(met.proc, n.adapt, n.chains){
     x[3, 1, s] ~ dpois(1) 
   }
   
-  logit(phi.11) <- phi.l.mu 
-  logit(phi.22) <- phi.n.mu
+  logit(phi.11) <- phi.l.mu
+  logit(phi.22) <- phi.n.mu 
   logit(l2n) <- grow.ln.mu
   logit(n2a) <- grow.na.mu
   
@@ -146,13 +143,14 @@ run_model <- function(met.proc, n.adapt, n.chains){
     theta.21[t] <- ifelse((gdd[t] >= 500) && (gdd[t] <= 2500),l2n,0)
     theta.32[t] <- ifelse((gdd[t] <= 1000) || (gdd[t] >= 2500),n2a,0)
     lambda[t] <- ifelse((gdd[t] >= 1500) && (gdd[t] <= 2500),repro.mu,0)
-      
+    
+
     for(s in 1:3){
       A.day[1,1,t,s] <- phi.11*(1-theta.21[t]) 
       A.day[2,1,t,s] <- phi.11*theta.21[t] 
-      A.day[2,2,t,s] <- phi.22*(1-theta.32[t]) + alpha.n[s]
+      A.day[2,2,t,s] <- phi.22*(1-theta.32[t]) 
       A.day[3,2,t,s] <- phi.22*theta.32[t]
-      logit(A.day[3,3,t,s]) <- phi.a.mu + alpha.a[s]
+      logit(A.day[3,3,t,s]) <- phi.a.mu[s]
       A.day[1,3,t,s] <- lambda[t]
       A.day[1,2,t,s] <- 0
       A.day[2,3,t,s] <- 0
@@ -206,10 +204,8 @@ run_model <- function(met.proc, n.adapt, n.chains){
       m[3,t,s] <- x[3,t,s]*b.adult[t,s] + 1E-10
       
       ## observation probability based on temperature
-      theta.larva[t,s] <- 1 / (1 + beta.l.obs*(temp.min[t])^2)
-      # theta.nymph[t] <- 1 / (1 + beta.n.obs*(met.obs[t])^2)
-      # theta.adult[t] <- 1 / (1 + beta.a.vert + beta.a.obs*(met.obs[t])^2)
-      
+      theta.larva[t,s] <- 1 / (1 + beta.l.obs*(temp.min[dt.index[s,t]])^2)
+
       ## binary outcome of observation by life stage
       b.larva[t,s] ~ dbern(theta.larva[t,s])
       b.nymph[t,s] ~ dbern(theta.nymph)
