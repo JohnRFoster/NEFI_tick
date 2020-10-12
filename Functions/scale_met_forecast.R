7adc15990f67a50c3abb3cdf81882bdb336eb7b6
John R Foster <fosterj@bu.edu>
2020-10-12 17:34:50 -0400

    means from historical fit

---
+++ Functions/scale_met_forecast.R
@@ -0,0 +1,15 @@
+library(plantecophys)
+library(dplyr)
+scale_met_forecast <- function(dir = ""){
+  met <- read.csv("/projectnb/dietzelab/fosterj/Data/Cary_Met_Data_Daily.csv")
+  met$DATE <- lubridate::mdy(met$DATE)
+  met <- met %>% 
+    mutate(vpd = RHtoVPD(met$MIN_RH, met$MIN_TEMP)) %>% 
+    filter(DATE >= "1995-01-01") %>% 
+    filter(DATE <= "2005-12-31") %>% 
+    select(c("MIN_TEMP","MAX_TEMP","MAX_RH","MIN_RH","vpd")) 
+  
+  met.means <- apply(met, 2, mean, na.rm = TRUE)
+  
+  return(met.means)
+} 
