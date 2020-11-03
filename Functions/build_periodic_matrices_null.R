2d4c511a81cd08b0ed4bc6c00f8884376700aa97
John R Foster <fosterj@bu.edu>
2020-11-03 16:42:02 -0500

    specify boot

--- Functions/build_periodic_matrices_null.R
+++ Functions/build_periodic_matrices_null.R
@@ -59,11 +59,11 @@ build_periodic_matrices_null <- function(ua, data, hb.site = NULL){
   # build matrices
   for(m in seq_along(ua$phi.a.mu)){
     
-    phi.11 <- inv.logit(ua$phi.l.mu[m])
-    phi.22 <- inv.logit(ua$phi.n.mu[m])
+    phi.11 <- boot::inv.logit(ua$phi.l.mu[m])
+    phi.22 <- boot::inv.logit(ua$phi.n.mu[m])
     lambda <- ifelse(gdd >= rho.l[m] & gdd <= 2500, ua$repro.mu[m], 0)
-    theta.32 <- ifelse(gdd <= 1000 | gdd >= rho.a[m], inv.logit(ua$grow.na.mu[m]), 0)
-    theta.21 <- ifelse(gdd >= rho.n[m] & gdd <= 2500, inv.logit(ua$grow.ln.mu[m]), 0)  
+    theta.32 <- ifelse(gdd <= 1000 | gdd >= rho.a[m], boot::inv.logit(ua$grow.na.mu[m]), 0)
+    theta.21 <- ifelse(gdd >= rho.n[m] & gdd <= 2500, boot::inv.logit(ua$grow.ln.mu[m]), 0)  
     
     # draw transition matrix with
     
@@ -71,7 +71,7 @@ build_periodic_matrices_null <- function(ua, data, hb.site = NULL){
     A[2,1,,m] <- phi.11*theta.21
     A[2,2,,m] <- phi.22*(1-theta.32) + alpha.n[m]
     A[3,2,,m] <- phi.22*theta.32
-    A[3,3,,m] <- inv.logit(ua$phi.a.mu[m] + alpha.a[m])
+    A[3,3,,m] <- boot::inv.logit(ua$phi.a.mu[m] + alpha.a[m])
     A[1,3,,m] <- lambda
   }
   return(A) 
