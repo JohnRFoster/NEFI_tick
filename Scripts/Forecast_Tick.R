486c2a827e0a1202eadecafded943cc4c31be82c
John R Foster <fosterj@bu.edu>
2020-11-05 15:35:53 -0500

    move qualifiers to the top, run arrays, set adapt to null, move climate ensembles to loop

--- Scripts/Forecast_Tick.R
+++ Scripts/Forecast_Tick.R
@@ -11,6 +11,10 @@
 #   the forecast period.                              #   
 # =================================================== #
 
+use.gefs <- TRUE # do we use gefs (if false we climate ensembles)
+weather.dir <- ifelse(use.gefs, "GEFS", "Climate")
+latency <- 31    # data latency
+
 library(rjags)
 library(tidyverse)
 library(lubridate)
@@ -37,17 +41,19 @@ date.pattern <- "\\d{4}-\\d{2}-\\d{2}"
 site.vec <- c("Green Control", "Henry Control", "Tea Control")
 
 ## read array job number and subset site
-# array.num <- as.numeric(Sys.getenv("SGE_TASK_ID"))
-array.num <- 2 # for testing 
+array.num <- as.numeric(Sys.getenv("SGE_TASK_ID"))
+# array.num <- 2 # for testing 
 site.name <- site.vec[array.num]
 
 cat("Running forecast on", site.name, "\n")
 
 ## parameter estimates from historical fit
 top.dir <- "../FinalOut/A_Correct"   # all fitted models start here
-model.type <- "RhoModels/RhoAllStart/MonthEffect" # path to specific model 
+model.type <- "RhoModels/RhoAllStart/MonthEffect/DiffPriors" # path to specific model 
 site.dir.name <- gsub(" Control", "", site.name) # site directory name
-rdata.name <- "Combined_thinMat_RhoStartMonthEffectLifeStage_HenryControl.RData" 
+rdata.name <- paste0("Combined_thinMat_RhoStartMonthEffect_LifeStageDiffPriors_", 
+                     gsub(" ", "", site.vec[array.num]), 
+                     ".RData") 
 load(file.path(top.dir,
                model.type,
                site.dir.name,
@@ -55,11 +61,13 @@ load(file.path(top.dir,
 
 # forecast output directory
 specific.name <- "LifeStageMonthEffect_UpdateAllParameters"
-today.dir <- paste0("Submit_", today())
-out.dir <- file.path("../FinalOut/ForecastTestRuns", today.dir, "Cary", 
+latency.dir <- paste0("dataLatency_", latency)
+out.dir <- file.path("../FinalOut/Forecast2020Cary", 
+                     site.dir.name,
                      model.type,
                      specific.name,
-                     site.dir.name)
+                     weather.dir,
+                     latency.dir) 
 
 # parameter estimates in data list, only Apr:Dec fitted during training
 data <- update_data(params.mat, 4:12) 
@@ -72,16 +80,15 @@ obs <- ticks %>%
   t()
 
 # data latency, days after observation data is "posted"
-latency <- 31  
 posted.days <- obs.dates + latency
 
 cat("Observations on:", as.character(obs.dates), "\n")
 cat("Posted on:", as.character(posted.days), "\n")
 
 # indexing for known met and gefs
-n.adapt <- 50#00
+n.adapt <- NULL
 n.chains <- 5
-n.iter <- 100#000
+n.iter <- 50000
 
 forecast.start.day <- "2020-05-19" # day forecast stems from
 every.day <- seq.Date(ymd(forecast.start.day), today(), by = 1) # everyday
@@ -114,6 +121,7 @@ ic.ticks <- bind_rows(train.ticks, ticks.06.18)
 ic.ticks <- round(apply(ic.ticks, 2, mean))
 
 data$ic <- ic.ticks # initial condition, put in data for jags
+y.vec <- rep(NA, 3) # no Ys at beginning of forecast, assimilated later
 
 # =================================================== #
 #       Observed met from Cary, previous months       #
@@ -123,6 +131,7 @@ met.cary <- read.csv("/projectnb/dietzelab/fosterj/Data/CaryCurrentYearMet.csv")
 met.cary$DATE <- mdy(met.cary$DATE)
 met.cary <- met.cary %>% 
   select(c("DATE","MAX_TEMP", "MIN_TEMP", "MAX_RH", "MIN_RH", "tot_prec")) 
+end.date.met.cary <- last(met.cary$DATE)
  
 # =================================================== #
 #       Observed met from Cary, current month         #
@@ -147,7 +156,8 @@ current.month <- webpage %>%
          "MIN_TEMP" = (MIN_TEMP-32)*5/9,
          "MAX_RH" = NA,                   # don't have RH for current month
          "MIN_RH" = NA,
-         "tot_prec" = tot_prec/25.4)      # in to mm
+         "tot_prec" = tot_prec/25.4)  %>%     # in to mm
+  filter(DATE > end.date.met.cary) # make sure we remove duplicate observations
 
 # combine to single data set
 met.cary <- bind_rows(met.cary, current.month)
@@ -168,19 +178,6 @@ met.cary$MIN_TEMP <- met.cary$MIN_TEMP - hist.means["MIN_TEMP"]
 met.cary$MAX_RH <- met.cary$MAX_RH - hist.means["MAX_RH"]
 met.cary$MIN_RH <- met.cary$MIN_RH - hist.means["MIN_RH"]
 met.cary$vpd <- met.cary$vpd - hist.means["vpd"]
-
-
-# =================================================== #
-#             Cary Climate Ensembles                  #
-# =================================================== #
-# need these ensembles to fill GEFS gaps and to run 
-# forecasts under climatology scenario 
-clim.max.temp <- cary_weather_ensembles("MAX_TEMP") 
-clim.min.temp <- cary_weather_ensembles("MIN_TEMP") 
-clim.max.rh <- cary_weather_ensembles("MAX_RH") 
-clim.min.rh <- cary_weather_ensembles("MIN_RH") 
-clim.vpd <- cary_weather_ensembles("vpd") 
-clim.cdd <- cary_weather_ensembles("cdd") 
 
 
 # =================================================== #
@@ -217,14 +214,17 @@ files.arch <- list.files(dir.gefs.arch, pattern = ".csv")
 dates.arch <- str_extract(files.arch, "^\\d{8}") %>% 
   ymd()
 
+
+
+
 # =================================================== #
 #                    FORECAST                         #
 # =================================================== #
 
 
 
-# for(i in seq_along(every.day)){
-i=1
+for(i in seq_along(every.day)){
+# i=1
   cat("\n\n===================================================\n\n")
   
   per <- i / length(every.day) * 100
@@ -245,32 +245,52 @@ i=1
     cat("New tick observation on", as.character(obs.dates[index]), "\n")
     cat("Posted on", as.character(posted.days[index]), "\n")
     
-    # grab forecast; all files written
-    files.forecast <- list.files(out.dir) 
+    # grab forecast from yesterday
+    yesterday <- current.day - 1
+    yest.dir <- file.path(out.dir, yesterday) # where forecasts are stored
+    files.forecast <- list.files(yest.dir) 
     
-    # the files that stem from previous forecast.start.day
-    forecast.group <- grep(forecast.start.day, files.forecast) 
-    files.forecast <- files.forecast[forecast.group] # subset
-    assim.file <- files.forecast[length(files.forecast)]
+    # assimilate forecast from climate ensembles
+    if(any(grepl("clim", files.forecast))){
+      files.forecast <- files.forecast[grep("clim", files.forecast)]
+      nc <- nc_open(file.path(yest.dir, files.forecast[1])) # read nc file
+      nrow <- nrow(ncvar_get(nc, "larvae")) # for dimensions
+      params.nc <- matrix(0, 1, length(params.nc.names))
+      colnames(params.nc) <- ncvar_get(nc, "parameter_names") # don't change
+      nc.larva <- nc.nymph <- nc.adult <- matrix(NA, nrow, length(files.forecast))
+      for(cf in seq_along(files.forecast)){
+        nc <- nc_open(file.path(yest.dir, files.forecast[cf])) # read nc file
+        f.days <- ncvar_get(nc, "time")     # dates for each forecast
+        f.2.grab <- which(current.day == f.days) # the day we need
+        nc.larva[,cf] <- ncvar_get(nc, "larvae")[,f.2.grab] # larva forecasts
+        nc.nymph[,cf] <- ncvar_get(nc, "nymph")[,f.2.grab]  # nymph forecasts
+        nc.adult[,cf] <- ncvar_get(nc, "adult")[,f.2.grab]  # adult forecasts
+        params.ens <- ncvar_get(nc, "parameter_samples")
+        colnames(params.ens) <- ncvar_get(nc, "parameter_names") # don't change
+        params.nc <- rbind(params.nc, params.ens)
+      }
+      params.nc <- params.nc[-1,] # remove first row from initialization above
+    } else {
+      nc <- nc_open(file.path(yest.dir, files.forecast[1])) # read nc file
+      f.days <- ncvar_get(nc, "time")     # dates for each forecast
+      f.2.grab <- which(current.day == f.days) # the day we need
+      nc.larva <- ncvar_get(nc, "larvae")[,f.2.grab] # larva forecasts
+      nc.nymph <- ncvar_get(nc, "nymph")[,f.2.grab]  # nymph forecasts
+      nc.adult <- ncvar_get(nc, "adult")[,f.2.grab]  # adult forecasts
+      params.nc <- ncvar_get(nc, "parameter_samples")
+      colnames(params.nc) <- ncvar_get(nc, "parameter_names") # don't change
+    }
     
-    load(file.path(out.dir, assim.file))
-    
-    # update posteriors 
-    data <- update_data(parameters, 1:12) 
-    
-    # pull out median predictions
-    forecast.med <- round(apply(preds, 2, median))
-    l.seq <- seq(1, by = 3, length.out = ncol(preds)/3) # larva columns
-    
-    larva <- forecast.med[l.seq]
-    nymph <- forecast.med[l.seq+1] # add 1 for nymph columns
-    adult <- forecast.med[l.seq+2] # add 2 for adult columns
-    
-    # find day that we need
-    median.index <- which(date.fore == new.start.day)
+    # pull out median predictions for DA
+    larva.m <- median(nc.larva)
+    nymph.m <- median(nc.nymph)
+    adult.m <- median(nc.adult)
+
+    # update posteriors
+    data <- update_data(params.nc, 1:12)	
     
     # initial condition is median of forecast
-    data$ic <- c(larva[median.index], nymph[median.index], adult[median.index])
+    data$ic <- c(larva.m, nymph.m, adult.m)
     
     # set forecast.start.day
     forecast.start.day <- new.start.day
@@ -286,70 +306,90 @@ i=1
   # needed for cum.gdd calculations across GEFS 
   end.cum.gdd <- build.obs.met$cdd[nrow(build.obs.met)]
   
-  # grab GEFS
-  gefs.needed <- ymd(current.day)+1
-  if(gefs.needed %in% dates.rnoaa){
-    met.gefs <- get_gefs_ens(gefs.forecast.dir, end.cum.gdd)
-  } else if(gefs.needed %in% dates.point){
-    # READ FROM POINT
-  } else if(gefs.needed %in% dates.grid){
-    # READ FROM GRID
-  } else if(gefs.needed %in% dates.arch){
-    # READ FROM ARCHIVES
-  }
-  
-  
-  
-  # if there has been a lapse in gefs
-  # need to remove the rows for which we have observations
-  if(gefs.diff > 0){
-    met.gefs <- modify(met.gefs, function(x) x[-c(1:gefs.diff),])
-    cat("There were missing GEFS files\n")
-  }
-  
-  # extract the mean vector and precision matrix for each weather variable
-  cum.gdd <- get_gefs_mean_prec(met.gefs, "cum.gdd") 
-  min.temp <- get_gefs_mean_prec(met.gefs, "min.temp", hist.means["MIN_TEMP"])
-  
-  # total days in forecast
-  data$n.days <- nrow(build.obs.met) + length(min.temp$mu)
-  
-  # need the month of all days in the sequence
-  all.days <- seq.Date(ymd(forecast.start.day), length.out = data$n.days, by = 1)
-  data$month.index <- month(all.days)
-  
-  # control flow for cumulative gdd priors if we drop 1st cum.gdd forecast
-  if(!is.null(cum.gdd$add.2.obs)){
-    data$cum.gdd <- c(pull(build.obs.met, cum.gdd), cum.gdd$add.2.obs)
-    
-    # need day of year for gefs prior
-    doys <- yday(all.days[-(1:(nrow(build.obs.met)+1))])
-    gefs.priors <- gefs_prior(doys)
-    data$cum.gdd.prior <- pull(gefs.priors, cum.gdd)
+  # grab appropriate GEFS
+  gefs.needed <- ymd(current.day)+1 # need gefs starting 'tomorrow'
+  if(use.gefs){
+    use.climate <- FALSE
+    cat("Weather forecast start day:", as.character(gefs.needed), "\n")
+    if(gefs.needed %in% dates.grid){
+      cat("Using GEFS from grid\n")
+      met.gefs <- get_gefs_noaaGEFSpoint("grid", gefs.needed, end.cum.gdd)
+    } else if(gefs.needed %in% dates.point){
+      cat("Using GEFS from point\n")
+      met.gefs <- get_gefs_noaaGEFSpoint("point", gefs.needed, end.cum.gdd)
+    } else if(gefs.needed %in% dates.arch){
+      cat("Using GEFS from archives\n")
+      met.gefs <- get_gefs_archive(gefs.needed, end.cum.gdd)
+    } else if(gefs.needed %in% dates.rnoaa){
+      cat("Using GEFS from rnoaa\n")
+      met.gefs <- get_gefs_rnoaa(gefs.needed, end.cum.gdd)
+    } else {
+      cat("No GEFS - using climate\n")
+      use.climate <- TRUE
+      clim.ens <- cary_weather_ensembles(yday(gefs.needed))
+    }  
   } else {
-    data$cum.gdd <- pull(build.obs.met, cum.gdd)  
+    cat("Using climatology ensembles\n")
+    use.climate <- TRUE
+    clim.ens <- cary_weather_ensembles(yday(gefs.needed))
+    # met.gefs <- met.gefs[sample(length(met.gefs), 7)]
+  }
+  
+  if(use.climate){
+    runs <- length(clim.ens) # the number of times we need to run the forecast - one per ensemble
+    data$cum.gdd <- pull(build.obs.met, cdd)
+ 
+    # total days in forecast
+    data$n.days <- nrow(build.obs.met) + nrow(clim.ens[[1]])
+    all.days <- seq.Date(ymd(forecast.start.day), length.out = data$n.days, by = 1)
+  } else {
     
-    # need day of year for gefs prior
+    runs <- 1 # the number of times we need to run the forecast - once
+    
+    # extract the mean vector and precision matrix for each weather variable
+    cum.gdd <- get_gefs_mean_prec(met.gefs, "cum.gdd") 
+    min.temp <- get_gefs_mean_prec(met.gefs, "min.temp", hist.means["MIN_TEMP"])
+    
+    # total days in forecast
+    data$n.days <- nrow(build.obs.met) + length(min.temp$mu)
+    all.days <- seq.Date(ymd(forecast.start.day), length.out = data$n.days, by = 1)
+    
+    # control flow for cumulative gdd priors if we drop 1st cum.gdd forecast
+    if(!is.null(cum.gdd$add.2.obs)){
+      data$cum.gdd <- c(pull(build.obs.met, cdd), cum.gdd$add.2.obs)
+      
+      # need day of year for gefs prior
+      doys <- yday(all.days[-(1:(nrow(build.obs.met)+1))])
+      gefs.priors <- gefs_prior(doys)
+      data$cum.gdd.prior <- pull(gefs.priors, cum.gdd)
+    } else {
+      data$cum.gdd <- pull(build.obs.met, cdd)  
+      
+      # need day of year for gefs prior
+      doys <- yday(all.days[-(1:nrow(build.obs.met))])
+      gefs.priors <- gefs_prior(doys)
+      data$cum.gdd.prior <- pull(gefs.priors, cum.gdd)
+    }
+    
+    # need day of year for gefs
     doys <- yday(all.days[-(1:nrow(build.obs.met))])
     gefs.priors <- gefs_prior(doys)
-    data$cum.gdd.prior <- pull(gefs.priors, cum.gdd)
+    data$obs.temp.prior <- pull(gefs.priors, min.temp)
+    
+    # number of days in each ensemble variable forecast
+    data$n.gefs.gdd <- length(cum.gdd$mu)
+    data$n.gefs.min.temp <- length(min.temp$mu)
+    
+    
+    data$cum.gdd.gefs.mu <- cum.gdd$mu
+    data$cum.gdd.gefs.prec <- cum.gdd$prec
+    data$obs.temp.gefs.mu <- min.temp$mu
+    data$obs.temp.gefs.prec <- min.temp$prec
   }
   
-  # need day of year for gefs
-  doys <- yday(all.days[-(1:nrow(build.obs.met))])
-  gefs.priors <- gefs_prior(doys)
-  data$obs.temp.prior <- pull(gefs.priors, min.temp)
-  
-  # number of days in each ensemble variable forecast
-  data$n.gefs.gdd <- length(cum.gdd$mu)
-  data$n.gefs.min.temp <- length(min.temp$mu)
-  
-  # build rest of data needed for jags
-  data$obs.temp <- pull(build.obs.met, TMIN)
-  data$cum.gdd.gefs.mu <- cum.gdd$mu
-  data$cum.gdd.gefs.prec <- cum.gdd$prec
-  data$obs.temp.gefs.mu <- min.temp$mu
-  data$obs.temp.gefs.prec <- min.temp$prec
+  # build data needed for jags
+  data$obs.temp <- pull(build.obs.met, MIN_TEMP)
+  data$month.index <- month(all.days)
   
   # build y matrix
   y <- matrix(NA, 3, data$n.days)
@@ -358,42 +398,74 @@ i=1
   
   cat("Forecast initial conditions and start day:", as.character(forecast.start.day), "\n")
   cat("Forecast is being run on:", as.character(current.day), "\n")
-  cat("Most recent weather observation:", as.character(last.noaa.date), "\n")
-  cat("The GEFS file assimilated:", days.2.grab, "\n")
   
-  # compile and sample
-  mcmc.model <- run_jagsFilter(data, n.adapt, n.chains, n.iter)
+  for(r in 1:runs){
+    
+    # control flow for file name and jags data for climate ensembles
+    if(use.climate){
+      cat("=== Fitting climate ensemble", r, "===\n")
+      data$clim.gdd.mu <- clim.ens[[r]] %>% pull(cum.gdd) 
+      data$clim.met.obs.mu <- clim.ens[[r]] %>% pull(min.temp) - hist.means["MIN_TEMP"]
+      
+      outname <- paste(forecast.start.day, 
+                       current.day, 
+                       paste0("clim", r),
+                       "Tick_forecast.nc", 
+                       sep = "_") 
+    } else {
+      outname <- paste(forecast.start.day, 
+                       current.day, 
+                       "Tick_forecast.nc", 
+                       sep = "_") 
+    }
+    
+    # compile and sample
+    mcmc.model <- run_jagsFilter(data, 
+                                 n.adapt, 
+                                 n.chains, 
+                                 n.iter, 
+                                 use.climate)
+    
+    # check convergence
+    out <- convergence_check(jags.out = mcmc.model$jags.out,
+                             model = mcmc.model$compiled.model,
+                             monitor = mcmc.model$monitor,
+                             n.iter = n.iter/2,
+                             min.eff.size = 2000)
+    
+    ## split output
+    # jags.out <- mcmc.model$jags.out
+    # out <- list(params = NULL, predict = NULL)
+    # mfit <- as.matrix(jags.out, chains = TRUE)
+    # pred.cols <- grep("x[", colnames(mfit), fixed = TRUE)
+    # chain.col <- which(colnames(mfit) == "CHAIN")
+    # out$predict <- ecoforecastR::mat2mcmc.list(mfit[, c(chain.col, pred.cols)])
+    # out$params <- ecoforecastR::mat2mcmc.list(mfit[, -pred.cols])
+    
+    # extract parameters and predicted states
+    params <- as.matrix(out$params)
+    preds <- as.matrix(out$predict)
+    
+    # create and check dir, each day the forecast is initiated gets own dir
+    out.dir.current.day <- file.path(out.dir, current.day)
+    if(!dir.exists(out.dir.current.day)) dir.create(out.dir.current.day, recursive = TRUE)
+    
+    # full path
+    ncfname <- file.path(out.dir.current.day, outname)
+    
+    # vector for what days tick data is assimilated (will need updating)
+    data.assimilation <- as.numeric(complete.cases(t(data$y)))
+    
+    # write netcdf 
+    create_ncdf_tick(ncfname = ncfname,
+                     preds = preds,
+                     params = params,
+                     start.date = forecast.start.day,
+                     data.assimilation = data.assimilation)
+
+  }
   
-  # check convergence
-  out <- convergence_check(jags.out = mcmc.model$jags.out,
-                           model = mcmc.model$compiled.model,
-                           monitor = mcmc.model$monitor,
-                           n.iter = n.iter/2,
-                           min.eff.size = 2000)
   
-  # extract parameters and predicted states
-  params <- as.matrix(out$params)
-  preds <- as.matrix(out$predict)
-  
-  # save as .RData
-  outname <- paste(forecast.start.day, current.day, "Tick_forecast_jagsFilter.nc", sep = "_") # name file
-  ncfname <- file.path(out.dir, outname)
-  
-  # vector for what days tick data is assimilated (will need updating)
-  data.assimilation <- as.numeric(complete.cases(t(data$y)))
-  
-  # write netcdf 
-  create_ncdf_tick(ncfname = ncfname,
-                   preds = preds,
-                   params = params,
-                   start.date = forecast.start.day,
-                   data.assimilation = data.assimilation)
-  
-  # check and create dir
-  if(!dir.exists(out.dir)) dir.create(out.dir, recursive = TRUE)
-  
-  save(preds, params, ncfname,
-       file = file.path(out.dir, "test.params.RData"))
   
   ## save forecasts within specific folders for each forecast date? or same directory?
 }
